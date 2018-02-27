88fc44d37adf4a1ee260a9d1cc57ac6ba75cdf01
Dan Wiedeback <dwiedeback@dexcom.com>
2018-02-26 23:28:47 -0800

    Header Extraction

---
+++ src/main/scala/com/dexcom/froutes/BaseRoutes.scala
@@ -0,0 +1,27 @@
+package com.dexcom.froutes
+
+import akka.http.scaladsl.model.{HttpEntity, HttpResponse}
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.{Route, RouteResult}
+
+import scala.concurrent.Future
+
+object BaseRoutes {
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  private val routeHandlingFunction: Route = { requestContext =>
+    val request = requestContext.request
+    val headers = request.headers
+    val responseString = headers.foldLeft("")((string, header) =>
+      string ++ s"Header name is ${header.name} with value ${header.value}\n")
+    val response = HttpResponse(entity = HttpEntity(responseString))
+    Future(RouteResult.Complete(response))
+  }
+
+  val routes: Route =
+    path("hello") {
+      get {
+        routeHandlingFunction
+      }
+    }
+}
--- src/main/scala/com/dexcom/froutes/FunctionRoutes.scala
+++ src/main/scala/com/dexcom/froutes/FunctionRoutes.scala
@@ -2,8 +2,6 @@ package com.dexcom.froutes
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
-import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 
 import scala.concurrent.ExecutionContextExecutor
@@ -17,16 +15,7 @@ object FunctionRoutes {
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
-    val route =
-      path("hello") {
-        get {
-          complete(
-            HttpEntity(ContentTypes.`text/html(UTF-8)`,
-                       "<h1>Say hello to akka-http</h1>"))
-        }
-      }
-
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+    val bindingFuture = Http().bindAndHandle(BaseRoutes.routes, "localhost", 8080)
 
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
