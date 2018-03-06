5a6c0505cf882089a72280ffcfbadb7aca87b400
Dan Wiedeback <dwiedeback@dexcom.com>
2018-03-06 13:09:13 -0800

    Cleanup

--- src/main/scala/com/dexcom/froutes/BaseRoutes.scala
+++ src/main/scala/com/dexcom/froutes/BaseRoutes.scala
@@ -1,22 +1,9 @@
 package com.dexcom.froutes
 
-import akka.http.scaladsl.model.{HttpEntity, HttpResponse}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
 
-import scala.concurrent.Future
-
 object BaseRoutes {
-  import scala.concurrent.ExecutionContext.Implicits.global
-
-  private val routeHandlingFunction: Route = { requestContext =>
-    val request = requestContext.request
-    val headers = request.headers
-    val responseString = headers.foldLeft("")((string, header) =>
-      string ++ s"Header name is ${header.name} with value ${header.value}\n")
-    val response = HttpResponse(entity = HttpEntity(responseString))
-    Future(RouteResult.Complete(response))
-  }
 
   private val extractHeadersFromRequest: RequestContext => String = { (requestContext: RequestContext) =>
     val request = requestContext.request
@@ -32,7 +19,7 @@ object BaseRoutes {
     path("hello") {
       get {
         routeHandlingDirective { headerString =>
-          complete("Lol I ignored the directive's result")
+          complete("Headers:\n" + headerString)
         }
       }
     }
